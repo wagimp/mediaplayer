ef74341d5e5b1f2035643af2922dca02ee1bce2c
Wagner Rodrigues da Silva <wagaum@gmail.com>
2026-07-01 19:41:02 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+import sys
+from PySide6.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QSlider, QFileDialog, QStyle)
+
+from  PySide6.QtCore import Qt, QUrl
+from PySide6.QtMultimedia import QMediaPlayer, QAudioOutput
